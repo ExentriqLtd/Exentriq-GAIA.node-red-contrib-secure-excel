99b3a0a2088c83dbc7bc23f9f862a7a1701b0996
Emilio Sabatucci <sabatucci@gmail.com>
2022-12-05 09:54:01 +0100

    first

---
+++ index.js
@@ -0,0 +1,44 @@
+const XlsxGenerator = require("xlsx-encrypter");
+
+module.exports = function(RED) {
+	
+    function SecureExcel(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+          
+            const password = msg.pwd;
+            const xlsxGenerator = new XlsxGenerator.XlsxGenerator();
+            /*const data = [
+			  {
+			    fruit: "Apples2",
+			    quantity: 4,
+			    price: "£6.86",
+			  },
+			];*/
+			
+			const data = msg.payload;
+			
+			const sheetName = msg.sheetName || "Sheet 1";
+			
+			// Create worksheets
+			const workSheet = xlsxGenerator.createWorkSheet(data, sheetName);
+			
+			// Create XLSX file
+			if(password){
+				void xlsxGenerator.exportWorkSheetsToFile(msg.filepath, [
+				  workSheet
+				],password);
+			}else{
+				void xlsxGenerator.exportWorkSheetsToFile(msg.filepath, [
+				  workSheet
+				]);
+			}
+			
+            
+            
+        });
+    }
+    
+    RED.nodes.registerType("secure-excel",SecureExcel);
+}
